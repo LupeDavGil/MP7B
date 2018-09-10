4bc5e4397509274135735e172a248ce173ca16af
LupeDavGil <guadalupe.davila@alumno.uttehuacan.edu.mx>
2018-09-10 09:22:48 -0500

    new file

--- HomeWork1/registrodegalonesaldia.java
+++ HomeWork1/registrodegalonesaldia.java
@@ -2,10 +2,18 @@ import java.util.Scanner;
 public class registrodegalonesaldia{
   public static void main(String[] args) {
     Scanner ingreso = new Scanner(System.in);
-    int PG, TG, GA,L;
+    int L;
+    double PG, TG, GA;
     System.out.println("Introdusca el numero de litros de leche vendidos:");
+    L = ingreso.nextInt();
+
     TG = (L / 3.785);
-    GA = ( PG * TG );
+    System.out.println("Galones que produce" + TG);
+
+    System.out.println("Introduce el precio por galon");
+    PG = ingreso.nextInt();
+
+    GA = TG * PG;
     System.out.println("Ganancia total del dia es:" + GA);
   }
 }
