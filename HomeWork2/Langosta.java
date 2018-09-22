ced3548ae0e2dbaa4af9d381cd4a23797f974708
MelinaSanc <melinitha21r@gmail.com>
2018-09-22 09:35:12 -0500

    new file

---
+++ HomeWork2/Langosta.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Langosta{
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int NP,TOT;
+
+System.out.println("Ingresa la cantidad de personas : ");
+NP = input.nextInt();
+if (NP>300){
+  TOT = NP * 75;
+  System.out.println("Total a pagar de la langosta : " + TOT);
+}else if (NP>200 && NP <=300){
+  TOT = NP * 85;
+  System.out.println("Total a pagar de la langosta : " + TOT);
+}else{
+  TOT = NP * 95;
+  System.out.println(" Total a pagar de la langosta : " + TOT);
+  }
+ }
+}
